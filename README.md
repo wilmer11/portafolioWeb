0bf0a08983476ebe7791d221090cb20ecde8089e
Wilmer A. Galeano <g_atoman@hotmail.com>
2018-11-01 22:58:39 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# Portafolio web
+
+Es un proyecto personal para dar a conocer los servicios de desarrollo web del creador
+
+- Pueden contactarme dentro la misma página o en https://wilmer11.herokuapp.com
+
+### Version
+
+1.0.0
+
+### Realizado 
+
+2018 Wilmer Alfredo Galeano
---
+++ js/main.js
@@ -0,0 +1,58 @@
+//botón fixed
+var a = document.getElementById('btn-toggle');
+function menus(){
+    var desplazamiento = window.pageYOffset;
+    if(desplazamiento <= 40){
+        a.classList.remove('btn-toggle2');
+        a.className = ('btn-toggle');
+        a.style.transition = '1s';
+    }else{
+       	a.classList.remove('btn-toggle');
+        a.className = ('btn-toggle2');
+        a.style.transition = '1s';
+    }
+}
+window.addEventListener('load', function(){  
+    $('#onload').fadeOut();
+    $('body').removeClass('hidden');
+    menus();
+});
+window.addEventListener('scroll', function() {
+    menus();
+});
+// botón en función
+$(document).ready(function(){
+	var est = true;
+	$('.ToggleSecction').slideDown();
+	if (est == true) {
+		$('body').css({
+			"overflow": "hidden",
+			"display": "block"
+		});
+	}
+});
+$(document).ready(function() {
+    var estado = false;
+    $('#btn-toggle').on('click', function() {
+        $('.ToggleSecction').slideToggle();
+        if (estado == true) {
+            $(this).text('Ver curriculum');
+            $('.btn-toggle').css({
+                "background": "rgba(248, 255, 42, .6)"
+            });
+            $('body').css({
+                "overflow": "hidden"
+            });
+            estado = false;
+        } else {
+            $(this).text('Cerrar');
+            $('.btn-toggle').css({
+                "background": "rgba(248, 255, 42, .6)"
+            });
+            $('body').css({
+                "overflow": "auto",
+            });
+            estado = true;
+        }
+    });
+});
